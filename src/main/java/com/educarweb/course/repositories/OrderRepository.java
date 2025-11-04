3533c68f73efb6898a95540398ce078ad3e68cac
Gustavo Burns Soares <gburnssoares@gmail.com>
2025-11-04 00:20:52 -0300

    add Order and OrderEnum

---
+++ src/main/java/com/educarweb/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.educarweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educarweb.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
