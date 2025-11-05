a44febf9248fe591e76e5776d4726bcd377ea0f8
Gustavo Burns Soares <gburnssoares@gmail.com>
2025-11-04 23:51:12 -0300

    Payment, OrderItem e associacoes

--- src/main/java/com/educarweb/course/config/TestConfig.java
+++ src/main/java/com/educarweb/course/config/TestConfig.java
@@ -10,10 +10,13 @@ import org.springframework.context.annotation.Profile;
 
 import com.educarweb.course.entities.Category;
 import com.educarweb.course.entities.Order;
+import com.educarweb.course.entities.OrderItem;
+import com.educarweb.course.entities.Payment;
 import com.educarweb.course.entities.Product;
 import com.educarweb.course.entities.User;
 import com.educarweb.course.entities.enums.OrderStatus;
 import com.educarweb.course.repositories.CategoryRepository;
+import com.educarweb.course.repositories.OrderItemRepository;
 import com.educarweb.course.repositories.OrderRepository;
 import com.educarweb.course.repositories.ProductRepository;
 import com.educarweb.course.repositories.UserRepository;
@@ -33,6 +36,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
+
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -62,9 +68,19 @@ public class TestConfig implements CommandLineRunner {
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
+
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 
 }
--- src/main/java/com/educarweb/course/entities/Order.java
+++ src/main/java/com/educarweb/course/entities/Order.java
@@ -1,18 +1,23 @@
 package com.educarweb.course.entities;
 
-
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.educarweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -32,6 +37,12 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Long getId() {
 		return id;
@@ -65,6 +76,18 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+
+	public Payment getPayment() {
+		return this.payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	public Order() {
