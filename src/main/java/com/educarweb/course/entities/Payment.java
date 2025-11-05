7b039905a92b8b786cc63be47ff7facfe78074d5
Gustavo Burns Soares <gburnssoares@gmail.com>
2025-11-05 00:00:27 -0300

    Metodo total e subtotal

--- src/main/java/com/educarweb/course/entities/Payment.java
+++ src/main/java/com/educarweb/course/entities/Payment.java
@@ -25,6 +25,7 @@ public class Payment implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
+	
 	@JsonIgnore
 	@OneToOne
 	@MapsId
